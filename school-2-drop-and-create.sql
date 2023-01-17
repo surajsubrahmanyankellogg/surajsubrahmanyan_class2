1ce277692b52d899bab5a2a451dd3cfc226f0c95
surajsubrahmanyankellogg <suraj.subrahmanyan@kellogg.northwestern.edu>
2023-01-17 20:04:12 +0000

    updated code

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,10 @@
 -- Drop the students table
+DROP TABLE IF EXISTS students;
 
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
+  email TEXT
+);
 -- Recreate it
---
+++ school-6-update.sql
@@ -0,0 +1,2 @@
+UPDATE students SET first_name = "Benjamin"
+WHERE id = 1;
